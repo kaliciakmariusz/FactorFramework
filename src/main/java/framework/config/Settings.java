e39eb9d5a79fcc7cbfef56010120a8777736debc
Factor <kaliciak.mariusz@gmail.com>
2018-04-14 15:27:22 +0200

    refactor

--- src/main/java/framework/config/Settings.java
+++ src/main/java/framework/config/Settings.java
@@ -4,13 +4,9 @@ import framework.base.BrowserType;
 
 public class Settings {
 
-    public static String AUTConnectionString;
-    public static String ExcelPath;
     public static String DriverType;
     public static String URLsite;
     public static BrowserType BrowserType;
-    public static String ExcelScenariosFile;
-    public static String ExcelDataFile;
     public static int BrowserImplicitlyTime;
     public static String ScreenshootFolderName;
     public static String LogsPath;
